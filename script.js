e2d6669fb6c801f0da563071e6f5b9a76731bd99
Ntungwe Irene <ntungwe@walulel.com>
2026-03-07 13:48:16 +0000

    added javascript file

---
+++ script.js
@@ -0,0 +1,24 @@
+// Subscribe button alert
+function subscribeMessage() {
+  alert("Thank you for subscribing.");
+}
+
+// Add to Cart alert
+function addToCart() {
+  alert("Item added to the cart.");
+}
+
+// Clear Cart alert
+function clearCart() {
+  alert("Cart cleared.");
+}
+
+// Process Order alert
+function processOrder() {
+  alert("Thank you for your order.");
+}
+
+// Contact form alert
+function submitMessage() {
+  alert("Thank you for your message.");
+}
